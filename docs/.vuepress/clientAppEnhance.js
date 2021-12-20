16b8d5eef7026383fcd234f75c35b95f8546e763
XIEYUANJIANG\xie <1099357646@qq.com>
2021-12-20 11:29:47 +0800

    docs:解决element-plus 加入语言包时 打包报错问题

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -1,13 +1,14 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-// import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+import zhCn from 'element-plus/es/locale/lang/zh-cn.mjs'
 
 import form from '../../packages/ins-form/lib/index.js'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.use(ElementPlus,{
-    // locale: zhCn,
+    locale: zhCn,
   })
   app.use(form)
   // ...
