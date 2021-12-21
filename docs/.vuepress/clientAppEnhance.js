029d1e429887e65c704dd89474af21a4b6b04157
XIEYUANJIANG\xie <1099357646@qq.com>
2021-12-21 15:13:22 +0800

    doc:修改vuepress对ins-form的引用

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -4,16 +4,18 @@ import 'element-plus/dist/index.css'
 
 import zhCn from 'element-plus/es/locale/lang/zh-cn.mjs'
 
-// import form from '../../packages/ins-form/lib/ins-form.es.js'
+import  InsForm from '@ins-ui/ins-form'
+// console.log('%c [ InsForm ]-8', 'font-size:13px; background:pink; color:#bf2c9f;', InsForm)
+// import InsForm from '../../packages/ins-form/lib/ins-form.es.js'
 // import '../../packages/ins-form/lib/css/index.css'
 
-import form from '../../packages/ins-form/dist/ins-form.es.js'
-import '../../packages/ins-form/dist/style.css'
+// import InsForm from '../../packages/ins-form/dist/ins-form.es.js'
+// import '../../packages/ins-form/dist/style.css'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.use(ElementPlus,{
     locale: zhCn,
   })
-  app.use(form)
+  app.use(InsForm)
   // ...
 })
