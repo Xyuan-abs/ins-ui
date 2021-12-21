efa54af184090a50b099b698165342bf14f6fa39
XIEYUANJIANG\xie <1099357646@qq.com>
2021-12-21 17:20:49 +0800

    fix:vuepress 添加ins-form的样式文件

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -5,12 +5,7 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn.mjs'
 
 import  InsForm from '@ins-ui/ins-form'
-// console.log('%c [ InsForm ]-8', 'font-size:13px; background:pink; color:#bf2c9f;', InsForm)
-// import InsForm from '../../packages/ins-form/lib/ins-form.es.js'
-// import '../../packages/ins-form/lib/css/index.css'
-
-// import InsForm from '../../packages/ins-form/dist/ins-form.es.js'
-// import '../../packages/ins-form/dist/style.css'
+import  '@ins-ui/ins-form/lib/style.css'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.use(ElementPlus,{
